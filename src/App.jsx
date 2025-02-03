4b2ed10294d89ec932775bb4823ac7212f13d7cb
waleedabdullah472 <waleedkhanii472@gmail.com>
2025-02-03 05:10:48 -0800

    Initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
 import PrivacyPolicy from './Components/Privacy';
 import './App.css';
 
